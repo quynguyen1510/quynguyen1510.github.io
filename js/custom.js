95809521e94347cf24759c2e16b660b16de8b898
Quy Nguyen <quycao1510@gmail.com>
2018-12-21 22:06:20 +0700

    -[Front-end]: update js for active navbar

--- js/custom.js
+++ js/custom.js
@@ -13,3 +13,14 @@ $('.nav-link').click(function(){
 	$('.collapse').slideUp();
 	$('#imgMenu').removeClass('active');
 });
+
+var container = document.getElementById("collapsibleNavbar");
+var navLink = container.getElementsByClassName('nav-link');
+
+for (var i = 0; i < navLink.length; i++) {
+	navLink[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
